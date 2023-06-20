a19f259ddd1e39c0d35268fdd4ac5bf462a3745c
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-06-20 22:42:27 +0800

    viper库的注意事项

--- settings/settings.go
+++ settings/settings.go
@@ -11,10 +11,11 @@ import (
 var Conf = new(AppConfig)
 
 type AppConfig struct {
-	Name         string `mapstructure:"name"`
-	Mode         string `mapstructure:"mode"`
-	Version      string `mapstructure:"version"`
-	Port         int    `mapstructure:"port"`
+	Name    string `mapstructure:"name"`
+	Mode    string `mapstructure:"mode"`
+	Version string `mapstructure:"version"`
+	Port    int    `mapstructure:"port"`
+
 	*LogConfig   `mapstructure:"log"`
 	*MySQLConfig `mapstructure:"mysql"`
 	*RedisConfig `mapstructure:"redis"`
@@ -47,11 +48,26 @@ type RedisConfig struct {
 }
 
 func Init() (err error) {
+	// 方式1：直接指定配置文件路径（相对路径或者绝对路径）
+	// 相对路径：相对执行的可执行文件的相对路径
+	// viper.SetConfigFile("./conf/config.yaml")
+	// 绝对路径：系统中实际的文件路径
+	// viper.SetConfigFile("/Users/qiaopengjun/Desktop/web_app2 /conf/config.yaml")
+
+	// 方式2：指定配置文件名和配置文件的位置，viper 自行查找可用的配置文件
+	// 配置文件名不需要带后缀
+	// 配置文件位置可配置多个
+	// 注意：viper 是根据文件名查找，配置目录里不要有同名的配置文件。
+	// 例如：在配置目录 ./conf 中不要同时存在 config.yaml、config.json
+
 	// 读取配置文件
 	viper.SetConfigFile("./config.yaml") // 指定配置文件路径
 	viper.SetConfigName("config")        // 配置文件名称(无扩展名)
-	viper.SetConfigType("yaml")          // SetConfigType设置远端源返回的配置类型，例如:“json”。
-	viper.AddConfigPath(".")             // 还可以在工作目录中查找配置
+	viper.AddConfigPath(".")             // 指定查找配置文件的路径（这里使用相对路径）可以配置多个
+	viper.AddConfigPath("./conf")        // 指定查找配置文件的路径（这里使用相对路径）可以配置多个
+	// SetConfigType设置远端源返回的配置类型，例如:“json”。
+	// 基本上是配合远程配置中心使用的，告诉viper 当前的数据使用什么格式去解析
+	viper.SetConfigType("yaml")
 
 	err = viper.ReadInConfig() // 查找并读取配置文件
 	if err != nil {            // 处理读取配置文件的错误
